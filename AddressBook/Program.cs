7b8964574b73ac3e98d9915b1a2bb1ab38b48a9a
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-10 08:23:07 +0530

    [SaiVenkat]-Creating New Contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,7 +6,8 @@ namespace AddressBook
         public static List<Contacts> contacts = new List<Contacts>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("Welcome to Address Book Program ");
+            Console.WriteLine("New Contact");
             AddContact addContact = new AddContact();
             AddressBook.AddContact.CreateContacts();
         }
